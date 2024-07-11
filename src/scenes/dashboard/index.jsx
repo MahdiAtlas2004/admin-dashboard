9eacda1c1b92c5d5e33d31283b3c735aca6d3f8b
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-07-11 09:02:08 +0430

    Manage Team section added

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,6 +1,17 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
 const Dashboard = () => {
     return ( 
-        <div>Dashboard</div>
+        <Box m="20px">
+            <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+            >
+                <Header title="Dashboard" subtitle="Welcome to your dashboard" />
+            </Box>
+        </Box>
      );
 }
  
